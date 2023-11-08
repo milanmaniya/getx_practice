3419df00de0ad8cf8607b018bda2bd6f287867a2
milanmaniya <milanmaniya2035@gmail.com>
2023-11-08 15:07:05 +0530

    named route done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
-import 'package:getx_practice/route.dart';
+import 'package:getx_practice/bottom_modal_sheet.dart';
+import 'package:getx_practice/named_route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      initialRoute: '/named',
+
+      getPages: [
+        GetPage(
+          name: '/named',
+          page: () => const NamedRouteDemo(),
+        ),
+        GetPage(
+          name: '/bottomsheet',
+          page: () => const BottomModalSheetDemo(),
+          
+        ),
+      ],
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const RouteDemo(),
+      
     );
   }
 }
---
+++ lib/named_route.dart
@@ -0,0 +1,34 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NamedRouteDemo extends StatefulWidget {
+  const NamedRouteDemo({super.key});
+
+  @override
+  State<NamedRouteDemo> createState() => _NamedRouteDemoState();
+}
+
+class _NamedRouteDemoState extends State<NamedRouteDemo> {
+  final name = 'jbfjd';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Get.toNamed(
+              '/bottomsheet?name=$name',
+              arguments: ['sdb', 'sbh'],
+            );
+            log(Get.arguments[0]);
+            log(Get.parameters['name']!);
+          },
+          child: const Text('Go to Named Route'),
+        ),
+      ),
+    );
+  }
+}
