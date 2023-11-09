f7b83bab15adb0fc591b78dc446b141ca62f0950
milanmaniya <milanmaniya2035@gmail.com>
2023-11-09 12:02:23 +0530

    dependency injection done

---
+++ lib/injection.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:getx_practice/getxController/controller.dart';
+
+class AllController extends GetxController {
+  get controller {
+    Get.put(
+      IncrementController(),
+      permanent: true,
+    );
+  }
+}
